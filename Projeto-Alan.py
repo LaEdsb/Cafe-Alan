6c6904480bb9719ffab8a6c450a0b3103f2bf3cb
Júlia Melo <melojulia686@gmail.com>
2024-06-14 01:27:31 -0300

    Função de adição de itens

--- Projeto-Alan.py
+++ Projeto-Alan.py
@@ -1,3 +1,13 @@
+def adicionar_item(menu):
+  categoria = int(input("Escolha a categoria:\n| 1 - entradas \n| 2 - pratos principais \n| 3 - sobremesas \n| 4 - bebidas \n "))
+  if categoria in menu:
+      item = input(f"\nDigite o item que você quer adicionar em {categoria}: ")
+      valor_item = float(input(f"\nDigite o valor que você quer adicionar em {item}: "))
+      menu[categoria].append({item: valor_item})
+      print(f"Item '{item}' adicionado à categoria {categoria}.")
+  else:
+      print("Categoria não encontrada.")
+      
 def buscar_itens(menu):
   busca = int(input("Escolha a categoria: \n | 1 - entradas \n| 2 - pratos principais \n| 3 - sobremesas \n| 4 - bebidas \n "))
   if busca in menu:
